d8217ed81e87a1fd28446d95dfd1219e0f8f98f1
MWPX777 <mwpx777@gmail.com>
2021-05-08 08:19:25 -0500

    finished with 22.1

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,14 +3,12 @@ import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 
 const Home = () => {
-  const [currentCategory, setCategory] = useState("");
+
 
   return (
     <div className="container">
-      <CategoryMenu setCategory={setCategory} />
-
-      {/* {currentCategory} is prop that is passed to ProductList */}
-      <ProductList currentCategory={currentCategory} />
+      <CategoryMenu />
+      <ProductList />
     </div>
   );
 };
